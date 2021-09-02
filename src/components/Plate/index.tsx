35e38502d4e2835c006910fa89562bc2ffd32242
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-09-01 22:14:07 -0300

    finish

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React from "react";
+import { MotiImage } from "moti";
 
-import { styles } from './styles';
-import plateImg from '../../assets/plate.png';
+import { styles } from "./styles";
+import plateImg from "../../assets/plate.png";
 
 export function Plate() {
   return (
-    <Image
+    <MotiImage
       style={styles.plate}
       source={plateImg}
       resizeMode="contain"
+      from={{ rotate: "180deg", opacity: 0 }}
+      animate={{
+        rotate: "0deg",
+        opacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 2000,
+        repeat: 3,
+        // repeatReverse: false, - padrao true, ele volta pra 0 caso vc coloque como false
+        // loop: true, fica infinito
+      }}
     />
   );
 }
--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,38 +1,99 @@
-import React from 'react';
-import { Feather } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
 
-import { styles } from './styles';
-import { theme } from '../../styles/theme';
+import { MotiView, useAnimationState, AnimatePresence } from "moti";
+
+import { styles } from "./styles";
+import { theme } from "../../styles/theme";
+import { timing } from "react-native-reanimated";
 
 export function Toggle() {
+  const [open, setOpen] = useState(false);
+
+  // mudar os estados da animacao
+  // oq vc cria ali dentro sao estados
+  // AnimatePresence pra ele nao sobreescrever outra visualizacao.
+  const toggleAnimationState = useAnimationState({
+    closed: {
+      height: 70,
+    },
+    open: {
+      height: 170,
+    },
+  });
+
+  const handleOpenToggle = () => {
+    toggleAnimationState.transitionTo("open");
+  };
+
+  const handleCloseToggle = () => {
+    toggleAnimationState.transitionTo("closed");
+  };
+
+  const handlePress = () => {
+    setOpen((prevState) => !prevState);
+    open
+      ? toggleAnimationState.transitionTo("open")
+      : toggleAnimationState.transitionTo("closed");
+  };
+
   return (
-    <View style={styles.container}>
-      <Feather
-        name="x"
-        color={theme.colors.white}
-        size={26}
-      />
+    <MotiView style={styles.container} state={toggleAnimationState}>
+      {/* <Pressable onPressIn={handleOpenToggle} onPressOut={handleCloseToggle}>
+        <Feather name="tag" color={theme.colors.white} size={26} />
+      </Pressable> */}
+
+      <TouchableOpacity onPress={handlePress}>
+        {open ? (
+          <AnimatePresence>
+            <MotiView
+              from={{
+                scale: 0,
+                opacity: 0,
+              }}
+              animate={{
+                scale: [
+                  { value: 0, type: "timing" },
+                  { value: 1.5, type: "spring" },
+                  { value: 1, type: "timing" },
+                ],
+                opacity: 1,
+              }}
+            >
+              <Feather name="tag" color={theme.colors.white} size={26} />
+            </MotiView>
+          </AnimatePresence>
+        ) : (
+          <MotiView
+            from={{
+              rotate: "0deg",
+              opacity: 0,
+            }}
+            animate={{
+              rotate: "90deg",
+              opacity: 1,
+            }}
+            transition={{
+              type: "timing",
+            }}
+          >
+            <Feather name="x" color={theme.colors.white} size={26} />
+          </MotiView>
+        )}
+      </TouchableOpacity>
 
       <View style={styles.info}>
-        <Text style={styles.label}>
-          Calories
-        </Text>
+        <Text style={styles.label}>Calories</Text>
 
-        <Text style={styles.value}>
-          150
-        </Text>
+        <Text style={styles.value}>150</Text>
       </View>
 
       <View style={styles.info}>
-        <Text style={styles.label}>
-          Weight
-        </Text>
+        <Text style={styles.label}>Weight</Text>
 
-        <Text style={styles.value}>
-          190g
-        </Text>
+        <Text style={styles.value}>190g</Text>
       </View>
-    </View >
+    </MotiView>
   );
 }
